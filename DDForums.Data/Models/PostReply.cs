87a843ad99bf62eb5d43976d35d4236be9b61669
delil97 <delil.demirsoy@hotmail.com>
2020-07-18 10:38:25 +0200

    Add PostController, service method

--- DDForums.Data/Models/PostReply.cs
+++ DDForums.Data/Models/PostReply.cs
@@ -6,7 +6,7 @@ namespace DDForums.Data.Models
     public class PostReply
     {
         public int Id { get; set; }
-        public string Context { get; set; }
+        public string Content { get; set; }
         public DateTime Created { get; set; }
 
         public virtual ApplicationUser User { get; set; }
--- DDForums.Service/PostService.cs
+++ DDForums.Service/PostService.cs
@@ -1,5 +1,6 @@
 ﻿using DDForums.Data;
 using DDForums.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,6 @@ namespace DDForums.Service
             _context = context; 
 
         }
-
         public Task Add(Post post)
         {
             throw new NotImplementedException();
@@ -39,7 +39,12 @@ namespace DDForums.Service
 
         public Post GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Posts.Where(post => post.Id == id)
+                .Include(post => post.User)
+                .Include(post => post.Replies).ThenInclude(reply => reply.User)
+                .Include(post => post.Forum)
+                .First();
+                
         }
 
         public IEnumerable<Post> GetFilteredPosts(string searchQuery)
